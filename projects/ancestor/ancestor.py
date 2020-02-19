12faeabf891d9444d3560ca928f0a73feca06aaf
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-02-18 16:39:06 -0800

    added comments

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,15 +1,13 @@
 from graph import Graph
-from util import Stack, Queue  # These may come in handy
 
 
 def earliest_ancestor(ancestors, starting_node, nodes=None):
-    # Use Depth First Search
-    # ancestors is an array of tuples
+    # Create a graph
     if nodes is None:
         nodes = set()
     # instantiate a graph
     graph = Graph()
-    # loop through array of edges
+    # loop through array of tuples
     for tuple in ancestors:
         # add numbers to set of possible nodes
         if tuple[0] not in nodes:
@@ -20,45 +18,37 @@ def earliest_ancestor(ancestors, starting_node, nodes=None):
     for node in nodes:
         graph.add_vertex(node)
     for tuple in ancestors:
-        # Add edges from tuples
+        # Add edges from tuples BACKWARDS
         graph.add_edge(tuple[1], tuple[0])
     # print(graph.vertices)
 
     path_list = []
-    no_none = []
     lengths = []
     possible = []
+
+    # find paths to every possible vertex
     for vertex in nodes:
         earliest = graph.dfs_recursive(starting_node, vertex)
-        path_list.append(earliest)
+        if earliest is not None:
+            path_list.append(earliest)
     # print(path_list)
 
-    for path in path_list:
-        if path is not None:
-            no_none.append(path)
-    # print(no_none)
-
-    for arr in no_none:
+    # Create list of the lengths of possible paths
+    for arr in path_list:
         lengths.append(len(arr))
     # print(lengths)
 
+    # Find the longest path
     longest = max(lengths)
-    print(longest)
-    for path in no_none:
+    # print(longest)
+    for path in path_list:
         if len(path) is longest:
+            # Return -1 if only possible path is the same
             if path[-1] is starting_node:
                 return(-1)
             else:
+                # Append possible ends to array
                 possible.append(path[-1])
+                # Grab the least of possible ends.
                 smallest = min(possible)
                 return(smallest)
-
-    # for path in no_none:
-    #     ends.append(path[-1])
-    # print(ends)
-
-    # print("earliest:", earliest)
-    # if earliest is None:
-    #     return -1
-    # else:
-    #     return earliest
