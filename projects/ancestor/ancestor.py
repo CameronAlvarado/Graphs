a1042ee7a4d53c9777aaa8753c4cd2b3d372cd36
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-02-18 16:56:15 -0800

    made code more dry

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,39 +1,32 @@
 from graph import Graph
 
 
-def earliest_ancestor(ancestors, starting_node, nodes=None):
-    # Create a graph
-    if nodes is None:
-        nodes = set()
+def earliest_ancestor(ancestors, starting_node):
     # instantiate a graph
     graph = Graph()
     # loop through array of tuples
     for tuple in ancestors:
         # add numbers to set of possible nodes
-        if tuple[0] not in nodes:
-            nodes.add(tuple[0])
-        if tuple[1] not in nodes:
-            nodes.add(tuple[1])
-    # Add nodes as vertices
-    for node in nodes:
-        graph.add_vertex(node)
+        if tuple[0] not in graph.vertices:
+            graph.add_vertex(tuple[0])
+        if tuple[1] not in graph.vertices:
+            graph.add_vertex(tuple[1])
+
+    # Add edges from tuples BACKWARDS
     for tuple in ancestors:
-        # Add edges from tuples BACKWARDS
         graph.add_edge(tuple[1], tuple[0])
     # print(graph.vertices)
 
+    # find paths to every possible vertex using recursion
     path_list = []
-    lengths = []
-    possible = []
-
-    # find paths to every possible vertex
-    for vertex in nodes:
+    for vertex in graph.vertices:
         earliest = graph.dfs_recursive(starting_node, vertex)
         if earliest is not None:
             path_list.append(earliest)
     # print(path_list)
 
     # Create list of the lengths of possible paths
+    lengths = []
     for arr in path_list:
         lengths.append(len(arr))
     # print(lengths)
@@ -48,6 +41,7 @@ def earliest_ancestor(ancestors, starting_node, nodes=None):
                 return(-1)
             else:
                 # Append possible ends to array
+                possible = []
                 possible.append(path[-1])
                 # Grab the least of possible ends.
                 smallest = min(possible)
