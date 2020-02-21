e9b98e56030ea52b50d68963c060853047ff66dd
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-02-20 22:14:17 -0800

    just saving where I left off

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from util import Stack
+from util import Stack, Queue
 from graph import Graph
 
 import random
@@ -34,61 +34,43 @@ traversal_path = []
 # You may find the commands `player.current_room.id`, `player.current_room.get_exits()` and `player.travel(direction)` useful.
 
 current_room = player.current_room.id
-# print("current room:", current_room)
+print("current room:", current_room)
+print()
 exits = player.current_room.get_exits()
-# print("exits:", exits)
+print("current room exits:", exits)
+print()
 random_exit = random.choice(exits)
-# print("random exit:", random_exit)
+print("random exit:", random_exit)
+print()
+rooms = []
+for room in room_graph:
+    rooms.append(room)
+print("Rooms:", rooms)
+print()
 print("Room Graph:", room_graph)
 print()
 
-# ----- Attempt 1 ------
-
-# instantiate a graph
-graph = Graph()
-
-for room in room_graph:
-    graph.add_vertex(room)
-
-for room in room_graph:
-    exit_dic = room_graph[room][-1]
-
-    for exit in exit_dic:
-        graph.add_edge(room, exit_dic[exit])
-print("Vertices:", graph.vertices)
-print()
-
-# find paths to every possible vertex using recursion
-path_list = []
-for vertex in graph.vertices:
-    path = graph.dfs_recursive(current_room, vertex)
-    # Filter out None values
-    if path is not None:
-        path_list.append(path)
-print("Path List:", path_list)
-print()
-
-# Convert path list into directions
-# opposite = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
-
-dir_list = []
-# exit_dic = room_graph[current_room][-1]
-
-opposite = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
-
-for path in path_list:
-    exit_dic = room_graph[current_room][-1]
-    for exit in exit_dic:
-        if exit_dic[exit] in path:
-            dir_list.append(exit)
-            current_room = exit_dic[exit]
-
-print(dir_list)
-
-# ----- End of Attempt 1 -----
-
 # Algorithm:
 # Use DFT
+q = Queue()
+# Add the starting vertex_id to the queue
+q.enqueue(current_room)
+# Create an empty set to store visited nodes
+visited = set()
+# While the queue is not empty...
+while q.size() > 0:
+    # Dequeue, the first vertex
+    room = q.dequeue()
+    # Check if it's been visited
+    # If it has not been visited...
+    if room not in visited:
+        # Mark it as visited
+        print(room)
+        visited.add(room)
+        # Then add all neighbors to the back of the queue
+        for neighbor in room_graph[current_room][-1]:
+            q.enqueue(room_graph[current_room][-1][neighbor])
+
 # Pick a random unexplored direction from current room
 # Travel and log that direction
 # Loop
@@ -100,6 +82,38 @@ print(dir_list)
 # Convert this to a list of n/s/e/w directions before adding to traversal path
 
 # Done when all paths have been explored
+
+# ----- Attempt 1 ------
+
+# instantiate a graph
+# graph = Graph()
+
+# for room in room_graph:
+#     graph.add_vertex(room)
+
+# for room in room_graph:
+#     exit_dic = room_graph[room][-1]
+
+#     for exit in exit_dic:
+#         graph.add_edge(room, exit_dic[exit])
+# print("Vertices:", graph.vertices)
+# print()
+
+# find paths to every possible vertex using recursion
+# paths = []
+# for vertex in graph.vertices:
+#     paths.append(graph.dfs(current_room, vertex))
+# print("Paths:", paths)
+# print()
+
+# dirs = []
+# for path in paths:
+#     for room in path:
+#         if room in room_graph[]
+# print(dirs)
+
+# ----- End of Attempt 1 -----
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
--- projects/adventure/graph.py
+++ projects/adventure/graph.py
@@ -65,6 +65,26 @@ class Graph:
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
+        # # Create an empty stack
+        # s = Stack()
+        # # Push the starting vertex_id to the stack
+        # s.push(starting_vertex)
+        # # Create an empty set to store visited nodes
+        # visited = set()
+        # # While the stack is not empty...
+        # while s.size() > 0:
+        #     # Pop the first vertex
+        #     v = s.pop()
+        #     # Check if it's been visited
+        #     # If it has not been visited...
+        #     if v not in visited:
+        #         # Mark it as visited
+        #         print(v)
+        #         visited.add(v)
+        #         # Then push all neighbors to the top of the stack
+        #         for neighbor in self.get_neighbors(v):
+        #             s.push(neighbor)
+        path = []
         # Create an empty stack
         s = Stack()
         # Push the starting vertex_id to the stack
@@ -79,11 +99,13 @@ class Graph:
             # If it has not been visited...
             if v not in visited:
                 # Mark it as visited
-                print(v)
+                # print(v)
+                path.append(v)
                 visited.add(v)
                 # Then push all neighbors to the top of the stack
                 for neighbor in self.get_neighbors(v):
                     s.push(neighbor)
+        return path
 
     def dft_recursive(self, starting_vertex, visited=None):
         """
